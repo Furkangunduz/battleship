6f81310abf1068ec6f22d7cbc4e7460cfc66a463
Furkan <furkan.gundz00@gmail.com>
2022-07-09 19:35:42 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+
 function App() {
-
   return (
-    <div>
-
-    </div>
-  );
+    <DndProvider backend={HTML5Backend}>
+      <Board key={"myboard"} enemyBoard={false} />
+      {/* <Board key={"enemyboard"} enemyBoard={true} /> */}
+      <Items />
+    </DndProvider>)
 }
 
 export default App;
