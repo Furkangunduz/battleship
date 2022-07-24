07272f9e6da671f4d88b30b3568966a22ecd2c4c
Furkan <furkan.gundz00@gmail.com>
2022-07-24 16:51:37 +0300

    userContext.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import { BrowserRouter } from "react-router-dom"
 
 import './style/home.css';
 import './style/board.css';
+import "./style/battle.css"
 import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { ShipProvider } from "./ShipContext"
 import { SocketProvider } from "./SocketContext"
+import { UserProvider } from './UserContext';
 
 import { ToastContainer } from 'react-toastify';
 import App from './App';
@@ -16,23 +18,26 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ShipProvider>
-    <BrowserRouter>
-      <SocketProvider>
-        <App />
-        <ToastContainer
-          position="bottom-center"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-        />
-      </SocketProvider>
-    </BrowserRouter>
-  </ShipProvider>
+  <UserProvider>
+
+    <ShipProvider>
+      <BrowserRouter>
+        <SocketProvider>
+          <App />
+          <ToastContainer
+            position="bottom-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
+        </SocketProvider>
+      </BrowserRouter>
+    </ShipProvider>
+  </UserProvider>
 );
 
