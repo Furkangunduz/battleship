2a875521a107ec6cbd9cc01e55647a9cc5da4c62
Furkan <furkan.gundz00@gmail.com>
2022-07-27 18:00:03 +0300

    Win page created.

--- src/components/Win.jsx
+++ src/components/Win.jsx
@@ -1,5 +1,28 @@
+import pirate from '../assets/chief.png';
+import { useContext } from 'react';
+import UserContext from '../UserContext';
+import BattleContext from '../BattleContext';
 function Win() {
-	return <div className='win'>Win</div>;
+	const { enemyName } = useContext(UserContext);
+	const { winner } = useContext(BattleContext);
+	return (
+		<div className='win'>
+			<div className='win-container'>
+				<div>
+					<img src={pirate} width='100%' height={'100%'} alt='asd' />
+				</div>
+				<div>
+					<div className='win-text'>
+						<p style={{ color: 'red', fontSize: '2rem' }}>
+							{winner == enemyName ? 'You Lose' : 'You win'}
+						</p>
+						<button disabled>Rematch</button>
+					</div>
+					<p>You win.</p>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Win;
